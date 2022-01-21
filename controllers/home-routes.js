934f0069e92ddf4de1c5a11ca7ffea5bf53e9ac6
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2022-01-21 13:23:02 -0500

    Added route for general posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -88,6 +88,54 @@ router.get('/post/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+//get general posts
+router.get('/post/:general/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+      general: req.params.topic
+    },
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at',
+      'topic',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username', 'admin']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username', 'admin']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+      console.log(post);
+      res.render('single-post', {
+        ...post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
